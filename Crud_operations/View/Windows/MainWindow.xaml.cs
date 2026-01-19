a7aa8c6810cdea22ad740c4dcf1a52172837f93b
vboxuser <vboxuser@WIN10>
2026-01-19 11:32:10 +0300

    Добавьте файлы проекта.

---
+++ Crud_operations/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Crud_operations.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ProductsLv.ItemsSource = App.context.Product.ToList();
+        }
+        
+        private void AddProductBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveProductBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditProductBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
